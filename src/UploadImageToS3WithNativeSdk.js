ddfe60602c788a017946bafc4e889fed8158e0d2
Harsshanth <73587980+Harsshanth@users.noreply.github.com>
2022-09-21 22:23:29 +0530

    TODO ui

--- src/UploadImageToS3WithNativeSdk.js
+++ src/UploadImageToS3WithNativeSdk.js
@@ -42,9 +42,24 @@ const UploadImageToS3WithNativeSdk = () => {
 
   return (
     <div>
-      <div>Native SDK File Upload Progress is {progress}%</div>
-      <input type="file" onChange={handleFileInput} />
-      <button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>
+      {/* <div>Native SDK File Upload Progress is {progress}%</div> */}
+      <center>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <input type="file" onChange={handleFileInput} /> <br></br>
+        <button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>
+      </center>
     </div>
   );
 };
